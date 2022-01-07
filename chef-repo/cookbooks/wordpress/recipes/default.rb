a37c6ec9b04740a81ac7c3e9142878d473caf236
Adrian Perez <adrianperezortega1997@gmail.com>
2022-01-07 22:07:51 +0100

    Added blog

--- chef-repo/cookbooks/wordpress/recipes/default.rb
+++ chef-repo/cookbooks/wordpress/recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe '::apache'
-include_recipe '::wordpress'
 include_recipe '::mysql'
+include_recipe '::wordpress'
 include_recipe '::facts'
 
   
--- chef-repo/cookbooks/wordpress/recipes/wordpress.rb
+++ chef-repo/cookbooks/wordpress/recipes/wordpress.rb
@@ -20,5 +20,23 @@ directory '/srv/www/' do
   end
 
   execute 'a2ensite wordpress' do
-    command 'a2ensite wordpress'
+    command 'a2ensite wordpress' 
+    notifies :restart, resources(:service => "apache2")
+  end
+
+  template "/vagrant/post.sql" do
+    source 'post.sql'
+  end
+  
+  chef_sleep 'name' do
+    seconds      5
+    action       :sleep 
+  end
+
+  execute 'configure wordpress' do
+    command 'curl http://192.168.33.40/wp-admin/install.php?step=2 --data-urlencode "weblog_title=DevOps" --data-urlencode "user_name=wordpress" --data-urlencode "admin_email=wordpress@unir.net" --data-urlencode "admin_password=wordpress" --data-urlencode "admin_password2=wordpress" --data-urlencode "pw_weak=1"'
+  end
+  
+  execute "update post" do
+    command "sudo mysql < /vagrant/post.sql"
   end
