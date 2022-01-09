f9b563f4f7162586f25025471674f9a61653c486
beaserter <beaserter@gmail.com>
2022-01-09 13:38:32 +0100

    Unit Test Coverage - Ubuntu

--- chef-repo/cookbooks/wordpress/spec/unit/recipes/default_spec.rb
+++ chef-repo/cookbooks/wordpress/spec/unit/recipes/default_spec.rb
@@ -7,29 +7,29 @@ describe 'wordpress::default' do
     #    end
     #end
     
-    context 'includes recipes apache' do
-        it 'includes the "wordpress::apache" recipe' do
-            expect(chef_run).to include_recipe('wordpress::apache')
-        end
-    end
+   # context 'includes recipes apache' do
+    #    it 'includes the "wordpress::apache" recipe' do
+     #       expect(chef_run).to include_recipe('wordpress::apache')
+      #  end
+    #end
 
-    context 'includes recipes mysql' do
-        it 'includes the "wordpress::mysql" recipe' do
-            expect(chef_run).to include_recipe('wordpress::mysql')
-        end
-    end
+    #context 'includes recipes mysql' do
+     #   it 'includes the "wordpress::mysql" recipe' do
+      #      expect(chef_run).to include_recipe('wordpress::mysql')
+       # end
+    #end
 
-    context 'includes recipes php' do
-        it 'includes the "wordpress::php" recipe' do
-            expect(chef_run).to include_recipe('wordpress::php')
-        end
-    end
+    #context 'includes recipes php' do
+        #it 'includes the "wordpress::php" recipe' do
+         #   expect(chef_run).to include_recipe('wordpress::php')
+        #end
+    #end
 
-    context 'includes recipes wordpress' do
-        it 'includes the "wordpress::wordpress" recipe' do
-            expect(chef_run).to include_recipe('wordpress::wordpress')
-        end
-    end
+    #context 'includes recipes wordpress' do
+     #   it 'includes the "wordpress::wordpress" recipe' do
+      #      expect(chef_run).to include_recipe('wordpress::wordpress')
+       # end
+    #end
 end
 
 describe 'wordpress::default' do
